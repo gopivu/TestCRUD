be94b8370af1773d99d2a3634958dce15e4ecdea
Gopi Krishna VURUKUTI <gopivu.in@mouritech.com>
2021-03-01 14:09:23 +0530

    #comments Arcitecture Modified.

---
+++ DataLayer/Class1.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace DataLayer
+{
+    public class Class1
+    {
+
+    }
+}
---
+++ Models/StudentDetails.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Models
+{
+    public class StudentDetails
+    {
+        public int StudentId { get; set; }
+        public string Name { get; set; }
+        public decimal Fees { get; set; }
+        public bool IsStaffReference { get; set; }
+
+    }
+}
