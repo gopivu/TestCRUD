e04b22136834612bf4d9a9ed3a024568ef1610db
Gopi Krishna VURUKUTI <gopivu.in@mouritech.com>
2021-03-01 13:34:40 +0530

    #Comments FirstCommit.

---
+++ TestCRUD/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace TestCRUD.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
